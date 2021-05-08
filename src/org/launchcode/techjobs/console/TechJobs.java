13c396ef233a28b4c3ae882a865a89f854467d6c
haley13 <allickhaley@gmail.com>
2021-05-08 11:34:26 -0500

    fixed duplicates

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -117,22 +117,29 @@ public class TechJobs {
         Scanner input = new Scanner(System.in);
         String stars = "*****";
 
+        if (someJobs.isEmpty()) {
+            System.out.println("No results");
+        }
+
         for (HashMap<String, String> jobInfo : someJobs) {//each job in someJobs AL
             //need 2 lines of code here, where do I want the stars to print, how where i want it to print relates to
             //these for loops; i want to print the stars after each job
-            if(jobInfo.size() == 5) {
-                jobInfo.put("",stars);
-            }
+            //if(jobInfo.size() == 5) {
+            System.out.println(stars);
+
 
             for (Map.Entry<String, String> jobData : jobInfo.entrySet()) {//each k,v pair in jobInfo
                 System.out.println(jobData.getKey() + ":" + jobData.getValue());
 
-
             }
 
         }
+
+
     }
 }
+
+
 
 
 
